29da1f2a2a573b46b41dba806aca5461e3ef389e
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-08-24 14:33:30 -0600

    Add files via upload

---
+++ EVAL1_HOLAMUNDO1/src/main/java/com/mycompany/eval1_holamundo1/EVAL1_HOLAMUNDO1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval1_holamundo1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVAL1_HOLAMUNDO1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
