4442224a2bcdaa9d3c7f2decef1418890b760214
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-08-29 07:34:41 -0600

    Add files via upload

---
+++ EVAL1_4_TV/src/main/java/com/mycompany/eval1_4_tv/EVAL1_4_TV.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval1_4_tv;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL1_4_TV {
+
+    public static void main(String[] args) {
+     
+        
+       String marca;
+       String modelo;
+       String tipo;
+       int tamaño;
+       double precio;
+       
+       
+       marca = ("Samsung");
+       modelo = ("Neo QLED");
+       tipo = ("OLED");
+       tamaño = 80;   
+       precio = 18000;
+         System.out.println(marca);
+           System.out.println(modelo);
+             System.out.println(tipo);
+             System.out.println(tamaño);
+               System.out.println(precio);
+       
+       
+    }
+}
