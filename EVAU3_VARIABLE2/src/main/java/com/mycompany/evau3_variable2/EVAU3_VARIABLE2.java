dab4c83d3c2d322d5a7b023002473c4166a53878
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-08-29 07:32:43 -0600

    Add files via upload

---
+++ EVAU3_VARIABLE2/src/main/java/com/mycompany/evau3_variable2/EVAU3_VARIABLE2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.evau3_variable2;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAU3_VARIABLE2 {
+
+    public static void main(String[] args) {
+        // TODO CODE APLICATION LOGIC CODE
+       String marca;
+       String modelo;
+       int year;// LA Ñ EN OTROS LENGUAJES NO ES ACEPATADA
+       double precio;
+       /// ASIGNACION
+       marca = "Nissan";
+       modelo= "GT";
+       year = 2008; 
+       precio = 1000000.1;
+           System.out.println(marca);
+                    System.out.println(modelo);
+                     System.out.println(year);
+                      System.out.println(precio);
+    }
+}
