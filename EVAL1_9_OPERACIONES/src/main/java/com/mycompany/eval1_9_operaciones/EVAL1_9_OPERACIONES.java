ac4f30ad93961d453c8719afc276259ce5110e4c
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-09-05 11:06:25 -0600

    Add files via upload

---
+++ EVAL1_9_OPERACIONES/src/main/java/com/mycompany/eval1_9_operaciones/EVAL1_9_OPERACIONES.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval1_9_operaciones;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL1_9_OPERACIONES {
+
+    public static void main(String[] args) {
+        // operaciones aritmeticas
+               int x=10;
+               int y=5;
+               int suma,resta,multi,div,pot,raiz;
+                      /// SUMA
+                       suma=x+y;
+                       System.out.println("Suma de x+y es:");
+                        System.out.println(suma);
+                              //RESTA
+                                resta=x-y;
+                                System.out.println("Resta de x-y es:");
+                                 System.out.println(resta);
+                                 //MULTIPLICACION
+                               multi=x*y;
+                                System.out.println("Multiplicacion de x*y es:");
+                                    System.out.println(multi);
+                                  ///DIVISION
+                               div=x/y;
+                                   System.out.println("Division de x/y es:");
+                                       System.out.println(div);
+                                       
+                                       x= 5;
+                                       y= 2;
+                                       div = x/y;
+                                       System.out.println("Division de x(5) / y(2);");
+                                                 System.out.println(div);
+                                                 /// DIVISION CON PUNTO
+                                                 
+                                                 double v11=5;
+                                                 double v12=2;
+                                                 double resu;
+                                                 resu= v11 / v12;
+                                                  System.out.println("Division de x(5) / y(2);");
+                                                 System.out.println(resu);
+                                                         
+                              
+                      
+       
+   }
+}
