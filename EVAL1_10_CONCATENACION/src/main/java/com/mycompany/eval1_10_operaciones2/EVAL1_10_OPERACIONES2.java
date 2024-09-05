0ffd6b4ca282e49d1843fc8260a5d9ea6ddb8334
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-09-05 11:06:50 -0600

    Add files via upload

---
+++ EVAL1_10_CONCATENACION/src/main/java/com/mycompany/eval1_10_operaciones2/EVAL1_10_OPERACIONES2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval1_10_operaciones2;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL1_10_OPERACIONES2 {
+
+    public static void main(String[] args) {
+       // CONCATENACION CADENAS:
+       //OPERADOR DE CONCATENACION
+       //MISMO SIMBOLO CON DIFERENTES USOS;
+       //SOBRE CARGA DE OPERADORES
+       String nombre = "Angel Axel";
+               String apellido = "Vega Rodriguez";
+               String NomCom;
+               NomCom= nombre + "  "+ apellido;
+                       System.out.println(NomCom);
+                       
+                       
+                      /// Imprimir datos
+               System.out.println("Nombre del usuario:"+nombre);
+               System.out.println("Apellido del usuario:"+apellido);
+               System.out.println("Nombre completo del usuario"+NomCom);
+       
+    }
+}
