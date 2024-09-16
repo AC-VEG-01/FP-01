f59956f2be5e3df7c4379b1dbbe2d66a67a41831
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-09-16 01:24:57 -0600

    Add files via upload

---
+++ EVAL1_15_AGUILA_SELLO/src/main/java/com/mycompany/eval1_15_aguila_sello/EVAL1_15_AGUILA_SELLO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval1_15_aguila_sello;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL1_15_AGUILA_SELLO {
+  
+    public static void main(String[] args) {
+     double  valor= Math.random();
+     if(valor>=0.5){
+         System.out.println("Aguila");
+     }else{
+                 System.out.println("Sello");
+                 }
+     }
+    
+}
