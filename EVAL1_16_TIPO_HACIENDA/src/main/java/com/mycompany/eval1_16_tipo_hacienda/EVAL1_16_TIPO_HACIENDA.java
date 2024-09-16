f63eded6ea18d29dae540773c4457468edf317ff
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2024-09-16 01:26:58 -0600

    Add files via upload

---
+++ EVAL1_16_TIPO_HACIENDA/src/main/java/com/mycompany/eval1_16_tipo_hacienda/EVAL1_16_TIPO_HACIENDA.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eval1_16_tipo_hacienda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author angel
+ */
+public class EVAL1_16_TIPO_HACIENDA {
+
+    public static void main(String[] args) {
+        char tipo;
+        Scanner cap=new Scanner (System.in);
+System.out.println("Tipo de persona F -- Fisica,Moral");
+tipo=cap.nextLine().charAt(0);
+System.out.println(tipo);
+
+    }
+}
